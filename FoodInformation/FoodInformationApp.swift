1e26de2e7430fd2a452823022eb5f8fafa71524a
Daniel Xu <pilot2035@gmail.com>
2024-08-05 13:43:38 -0400

    Initial commit

---
+++ FoodInformation/FoodInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodInformationApp.swift
+//  FoodInformation
+//
+//  Created by Daniel Xu on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
